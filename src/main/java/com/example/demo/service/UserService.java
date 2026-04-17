0d4ea09b3e7780c1dad28d064cfffdaddc37d5ae
TrgMSon <truongminhson1810@gmail.com>
2026-04-17 16:48:06 +0700

    merge manage reader and user

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,17 +1,23 @@
 package com.example.demo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.BorrowCard;
 import com.example.demo.model.User;
+import com.example.demo.repository.BorrowCardRepo;
 import com.example.demo.repository.UserRepo;;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private BorrowCardRepo borrowCardRepo;
 
     public User findUserByEmail(String email, String password) {
         User result = userRepo.findByEmail(email, password);
@@ -26,5 +32,20 @@ public class UserService {
 
     public void saveUser(User user) {
         userRepo.save(user.getEmail(), user.getName(), user.getPassword(), "user");
+    }
+
+    public void deleteUser(int id) {
+        User user = userRepo.findById(id).orElse(null);
+        if (user != null) {
+            List<BorrowCard> borrowCards = borrowCardRepo.findByUser(user);
+            if (borrowCards != null) {
+                for (BorrowCard borrowCard : borrowCards) {
+                    borrowCard.setUser(null);
+                }
+
+                borrowCardRepo.saveAll(borrowCards);
+                userRepo.delete(user);
+            }
+        }
     }
 }
